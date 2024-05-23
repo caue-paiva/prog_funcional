86ac27063096c52a544a9d904b230d458d8f6494
“Caue <cauepaivalira@outlook.com>
2024-05-23 09:02:41 -0300

    tentando fazer a parte de printar

--- python/trabalho4.py
+++ python/trabalho4.py
@@ -62,31 +62,49 @@ def calcula_bonus(list_bonus:list[int], opcao:int)->list[int]: #0 -> norma, 1 ->
 def calcula_pontos(lista_jogadas:list[int])->int:
     return jogadas_aux(lista_jogadas,0,[0],-1,1,0)  
 
-def string_jogada_aux(list_jogadas:list[int], index:int, arremeso_ante_rodada:int, string_retorno:str)->str:
+def string_jogada_aux(
+      list_jogadas:list[int],
+      index:int, 
+      arremeso_ante_rodada:int, 
+      string_retorno:str,
+      rodada_atual:int
+   )->str:
    if index >= len(list_jogadas):
       return string_retorno
 
    pinos_derrubados: int = list_jogadas[index] 
-   novo_arremeso_ante:int
 
-   if arremeso_ante_rodada == -1: #primeira jogada
+   if rodada_atual < 10 :
+      if arremeso_ante_rodada == -1: #primeira jogada
+         if pinos_derrubados == 10:
+            nova_string = string_retorno + "X _ | "
+            novo_arremeso_ante = -1 #vamos para uma nova rodada depois do strike
+            prox_rodada = rodada_atual + 1
+         else:
+            novo_arremeso_ante = pinos_derrubados
+            nova_string = string_retorno + f"{pinos_derrubados} "
+            prox_rodada = rodada_atual
+      else: #segunda jogada
+         novo_arremeso_ante = -1
+         if arremeso_ante_rodada + pinos_derrubados == 10: #spare
+            nova_string = string_retorno + "/ | "
+         else:
+            nova_string = string_retorno + f"{pinos_derrubados} | "
+         prox_rodada = rodada_atual + 1
+   else:
+      
       if pinos_derrubados == 10:
-         nova_string = string_retorno + "X _ | "
-         novo_arremeso_ante = -1 #vamos para uma nova rodada depois do strike
+         nova_string = string_retorno + "X "
       else:
-         novo_arremeso_ante = pinos_derrubados
-         nova_string = string_retorno + f"{pinos_derrubados} "
-   else: #segunda jogada
-      novo_arremeso_ante = -1
-      if arremeso_ante_rodada + pinos_derrubados == 10: #spare
-         nova_string = string_retorno + "/ | "
-      else:
-         nova_string = string_retorno + f"{pinos_derrubados} | "
-         
-   return string_jogada_aux(list_jogadas,index+1,novo_arremeso_ante,nova_string)
+         nova_string = string_retorno + f"{pinos_derrubados}"
+      
+      novo_arremeso_ante = pinos_derrubados
+      prox_rodada = rodada_atual
+
+   return string_jogada_aux(list_jogadas,index+1,novo_arremeso_ante,nova_string,prox_rodada)
 
 def string_jogada(list_jogada:list[int])->str:
-   return string_jogada_aux(list_jogada,0,-1,"") + "|"
+   return string_jogada_aux(list_jogada,0,-1,"",1) + "|"
 
 jogadas:list = [ 1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6 ]
 jogadas2:list = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
